367c26083af3f053d3122bf09d041f4f378df430
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2019-12-16 02:00:00 +0100

    Initial commit

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+function on() {
+    document.getElementById("overlay2").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay2").style.display = "none";
+  }
